d645f8da2bafde27ab21ba434cd0d3429a56f590
Stefan Strobl <stefan@strobl.systems>
2016-11-30 23:02:06 +0100

    remove css

--- src/components/ng2-datepicker.ts
+++ src/components/ng2-datepicker.ts
@@ -73,7 +73,7 @@ export class DatePickerComponent implements ControlValueAccessor, OnInit {
     this.opened = this.opened || false;
     this.format = this.format || 'YYYY-MM-DD';
     this.viewFormat = this.viewFormat || 'D MMMM YYYY';
-    this.firstWeekdaySunday = this.firstWeekdaySunday || false; 
+    this.firstWeekdaySunday = this.firstWeekdaySunday || false;
     setTimeout(() => {
       if (!this.viewDate) {
         let value = moment();
@@ -108,10 +108,10 @@ export class DatePickerComponent implements ControlValueAccessor, OnInit {
     for (let i = n; i <= date.endOf('month').date(); i += 1) {
       let currentDate = moment(`${i}.${month + 1}.${year}`, 'DD.MM.YYYY');
       let today = (moment().isSame(currentDate, 'day') && moment().isSame(currentDate, 'month')) ? true : false;
-      let selected = (selectedDate.isSame(currentDate, 'day')) ? true : false; 
+      let selected = (selectedDate.isSame(currentDate, 'day')) ? true : false;
 
       if (i > 0) {
-        this.days.push({ 
+        this.days.push({
           day: i,
           month: month + 1,
           year: year,
@@ -120,13 +120,13 @@ export class DatePickerComponent implements ControlValueAccessor, OnInit {
           selected: selected
         });
       } else {
-        this.days.push({ 
+        this.days.push({
           day: null,
           month: null,
           year: null,
           enabled:false,
           today: false,
-          selected: false 
+          selected: false
         });
       }
     }
@@ -137,6 +137,11 @@ export class DatePickerComponent implements ControlValueAccessor, OnInit {
 
     let date: CalendarDate = this.days[i];
     let selectedDate = moment(`${date.day}.${date.month}.${date.year}`, 'DD.MM.YYYY');
+
+
+    if (!selectedDate.isValid())
+      return;
+
     this.value = selectedDate.format(this.format);
     this.viewDate = selectedDate.format(this.viewFormat);
     this.close();
@@ -166,7 +171,7 @@ export class DatePickerComponent implements ControlValueAccessor, OnInit {
   }
 
   toggle() {
-    this.opened = !this.opened; 
+    this.opened = !this.opened;
   }
 
   open() {
